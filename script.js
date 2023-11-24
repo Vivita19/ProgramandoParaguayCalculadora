e2b51ef1c45d19fe82d42683dfe040a1a2af86ae
Vivita19 <151692976+Vivita19@users.noreply.github.com>
2023-11-23 21:22:06 -0300

    empezando con JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+let peso;
+peso = 15;
+console.log(peso+1);
+let miVariable = "1";
+let volumen = 1600;
+const Mi_constante=10;
+let unidad="cc";
+console.log("volumen diario:"+volumen+unidad)
+
+console.log(peso);
