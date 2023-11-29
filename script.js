9af9205d94d2e3c8e6f57ef36503f1af53f1fd98
Vivita19 <151692976+Vivita19@users.noreply.github.com>
2023-11-28 23:23:02 -0300

    finalizado?; pero, funciona

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-let peso;
-peso = 15;
-console.log(peso+1);
-let miVariable = "1";
-let volumen = 1600;
-const Mi_constante=10;
-let unidad="cc";
-console.log("volumen diario:"+volumen+unidad)
+const CALCULAR = document.getElementById('calcular');
+const ERROR = document.getElementById('error');
+const FLU = document.getElementById('flu');
+const MAN = document.getElementById('man');
 
-console.log(peso);
+CALCULAR.addEventListener('click', () => {
+    const DATO = document.getElementById('peso').value
+    //validamos que se cargue un dato:
+    if (DATO > 0){
+        ERROR.style.display = 'none'
+        let flujo = calcFlujo(DATO);
+        let mantenimiento = flujo*1.5;
+        FLU.innerHTML = flujo + ' cc/hr';
+        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+    } else {
+        ERROR.style.display = 'block';
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
+    }
+})
+function calcFlujo(peso){
+    let resto = peso;
+    let flujo = 0;
+    if (resto>20){
+        let aux = resto-20;
+        flujo += aux*1;
+        resto -= aux;
+    } 
+    if (resto>10){
+        let aux = resto-10;
+        flujo += aux*2;
+        resto -= aux;
+    }
+    flujo += resto*4;
+    return flujo;
+}
